7739b751218f22855866414f651e121ba5b2d16a
Andrea Castello <castello.a@gmail.com>
2020-02-21 12:24:05 +0100

    WPC-194: event-sender app for .NET Core

---
+++ event-streams-dotnet/event-sender/EventData.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace event_sender
+{
+    class EventData
+    {
+        [JsonProperty("X-Ucbrowser-Ua")]
+        private String ucBrowserUa;
+
+        [JsonProperty("X-Ucbrowser-Device-Ua")]
+        private String ucBrowserDeviceUa;
+
+        [JsonProperty("Accept-Language")]
+        private String acceptLanguage;
+
+        [JsonProperty("Accept-Encoding")]
+        private String acceptEncoding;
+
+        [JsonProperty("Accept-Charset")]
+        private String acceptCharset;
+
+        [JsonProperty("X-Forwarded-For")]
+        private String xForwardedFor;
+
+        [JsonProperty("Accept")]
+        private String accept;
+
+        [JsonProperty("User-Agent")]
+        private String userAgent;
+
+        [JsonProperty("Video_id")]
+        private String videoID;
+
+        [JsonProperty("Clientip")]
+        private String clientIP;
+
+        [JsonProperty("Event")]
+        private String eventResult;
+
+        [JsonProperty("X-Operamini-Features")]
+        private String xOperaminiFeatures;
+
+        [JsonProperty("X-Operamini-Phone")]
+        private String xOperaminiPhone;
+
+        [JsonProperty("X-Operamini-Phone-Ua")]
+        private String xOperaminiPhoneUa;
+
+        [JsonProperty("Save-Data")]
+        private String saveData;
+
+        [JsonProperty("X-Clacks-Overhead")]
+        private String xClacksOverhead;
+
+        [JsonProperty("Sec-Fetch-Site")]
+        private String secFetchSite;
+
+        [JsonProperty("Sec-Fetch-Mode")]
+        private String secFetchMode;
+
+        [JsonProperty("Request-Context")]
+        private String requestContext;
+
+        [JsonProperty("Correlation-Context")]
+        private String correlationContext;
+
+        [JsonProperty("X-Ms-Request-Root-Id")]
+        private String xMsRequestRootID;
+
+        [JsonProperty("X-Ms-Request-Id")]
+        private String xMsRequestID;
+
+        // timestamp in nanoseconds, when the event is sent to output (not in the original stream)
+        [JsonProperty("timestamp")]
+        private long timestamp;
+
+        public void SetTimestamp(long timestamp)
+        {
+            this.timestamp = timestamp;
+        }
+    }
+}
---
+++ event-streams-dotnet/event-sender/Sender.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+
+
+namespace event_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
+            var path = @"..\..\..\event_stream.json";
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Unable to open json event stream, file does not exist");
+                Thread.Sleep(2000);
+                Environment.Exit(1);
+            }
+            var json = File.ReadAllText(path);
+            var events = JsonConvert.DeserializeObject<EventData[]>(json);
+            foreach(EventData ev in events)
+            {
+                // Adds a timestamp, serializes events again and sends it to the stdout one by one
+                ev.SetTimestamp(GetTimestamp());
+                Console.WriteLine(JsonConvert.SerializeObject(ev));
+                Thread.Sleep(100);
+            }
+
+        }
+
+        // gets a timestamp in nanoseconds
+        static long GetTimestamp()
+        {
+            var timestamp = Stopwatch.GetTimestamp();
+            var nanoseconds = 1_000_000_000.0 * timestamp / Stopwatch.Frequency;
+
+            return (long) nanoseconds;
+        }
+    }
+}

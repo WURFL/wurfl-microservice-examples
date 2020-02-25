7e2fe43cc09d7e0fb10bc48d25a1a6176fd4a786
Andrea Castello <castello.a@gmail.com>
2020-02-25 09:18:45 +0100

    Added event-processor dotnet core code

---
+++ event-streams-dotnet/event-processor/EnrichedEventData.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace event_processor
+{
+    class EnrichedEventData
+    {
+        [JsonProperty("X-Ucbrowser-Ua")]
+        private String ucBrowserUa;
+
+        [JsonProperty("X-Ucbrowser-Device-Ua")]
+        private String ucBrowserDeviceUa;
+
+        [JsonProperty("Accept-Language")]
+        private String acceptLanguage;
+
+        [JsonProperty("Accept-Encoding")]
+        private String acceptEncoding;
+
+        [JsonProperty("Accept-Charset")]
+        private String acceptCharset;
+
+        [JsonProperty("X-Forwarded-For")]
+        private String xForwardedFor;
+
+        [JsonProperty("Accept")]
+        private String accept;
+
+        [JsonProperty("User-Agent")]
+        private String userAgent;
+
+        [JsonProperty("Video_id")]
+        private String videoID;
+
+        [JsonProperty("Clientip")]
+        private String clientIP;
+
+        [JsonProperty("Event")]
+        private String eventResult;
+
+        [JsonProperty("X-Operamini-Features")]
+        private String xOperaminiFeatures;
+
+        [JsonProperty("X-Operamini-Phone")]
+        private String xOperaminiPhone;
+
+        [JsonProperty("X-Operamini-Phone-Ua")]
+        private String xOperaminiPhoneUa;
+
+        [JsonProperty("Save-Data")]
+        private String saveData;
+
+        [JsonProperty("X-Clacks-Overhead")]
+        private String xClacksOverhead;
+
+        [JsonProperty("Sec-Fetch-Site")]
+        private String secFetchSite;
+
+        [JsonProperty("Sec-Fetch-Mode")]
+        private String secFetchMode;
+
+        [JsonProperty("Request-Context")]
+        private String requestContext;
+
+        [JsonProperty("Correlation-Context")]
+        private String correlationContext;
+
+        [JsonProperty("X-Ms-Request-Root-Id")]
+        private String xMsRequestRootID;
+
+        [JsonProperty("X-Ms-Request-Id")]
+        private String xMsRequestID;
+
+        // timestamp in nanoseconds, when the event is sent to output (not in the original stream)
+        [JsonProperty("timestamp")]
+        private long timestamp;
+
+        public void SetTimestamp(long timestamp)
+        {
+            this.timestamp = timestamp;
+        }
+
+        // Device detection fields
+        [JsonProperty("Wurfl-Complete-Name")]
+        private String wurflCompleteName;
+        [JsonProperty("Wurfl-Device-OS")]
+        private String wurflDeviceOS;
+        [JsonProperty("Wurfl-Form-Factor")]
+        private String wurflFormFactor;
+        [JsonProperty("Wurfl-Device-Make")]
+        private String wurflDeviceMake;
+        [JsonProperty("Wurfl-Device-Model")]
+        private String wurflDeviceModel;
+
+        // Accessor properties
+        public string UserAgent { get => userAgent; set => userAgent = value; }
+        public string WurflCompleteName { get => wurflCompleteName; set => wurflCompleteName = value; }
+        public string WurflDeviceOS { get => wurflDeviceOS; set => wurflDeviceOS = value; }
+        public string WurflDeviceMake { get => wurflDeviceMake; set => wurflDeviceMake = value; }
+        public string WurflFormFactor { get => wurflFormFactor; set => wurflFormFactor = value; }
+        public string WurflDeviceModel { get => wurflDeviceModel; set => wurflDeviceModel = value; }
+        public string UcBrowserUa { get => ucBrowserUa; set => ucBrowserUa = value; }
+        public string UcBrowserDeviceUa { get => ucBrowserDeviceUa; set => ucBrowserDeviceUa = value; }
+        public string AcceptLanguage { get => acceptLanguage; set => acceptLanguage = value; }
+        public string Accept { get => accept; set => accept = value; }
+        public string XOperaminiPhoneUa { get => xOperaminiPhoneUa; set => xOperaminiPhoneUa = value; }
+    }
+}
---
+++ event-streams-dotnet/event-processor/Processor.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Wmclient;
+
+namespace event_processor
+{
+    class Processor
+    {
+        static void Main(string[] args)
+        {
+            var errors = new List<String>();
+
+            String host = "localhost";
+            switch (args.Length)
+            {
+                case 0:
+                    // keeps default host
+                    break;
+                case 2:
+                    host = args[1];
+                    break;
+                default:
+                    Console.WriteLine("Usage: event-processor-demo [--host <WURFL microservice serve IP>]");
+                    Thread.Sleep(2000);
+                    Environment.Exit(1);
+                    break;
+            }
+
+            WmClient wmClient = null;
+            try
+            {
+                wmClient = WmClient.Create("http", host, "8080", "");
+                wmClient.SetCacheSize(20000);
+            }
+            catch (WmException e)
+            {
+                Console.WriteLine("Unable to connect to host: " + host);
+                Console.WriteLine(e.Message);
+                Thread.Sleep(2000);
+                Environment.Exit(1);
+            }
+            JsonSerializer serializer = new JsonSerializer();
+
+
+            var inp = Console.In;
+            JsonTextReader reader = new JsonTextReader(inp);
+            reader.SupportMultipleContent = true;
+            while (true)
+            {
+                if (!reader.Read())
+                {
+                    break;
+                }
+                EnrichedEventData eventData = serializer.Deserialize<EnrichedEventData>(reader);
+           
+                try
+                {
+                    // Simulate data coming from an HTTP request
+                    JSONDeviceData device = wmClient.LookupRequest(CreateRequest(eventData));
+                    eventData.WurflCompleteName = device.Capabilities["complete_device_name"];
+                    eventData.WurflDeviceMake = device.Capabilities["brand_name"];
+                    eventData.WurflDeviceModel = device.Capabilities["model_name"];
+                    eventData.WurflFormFactor = device.Capabilities["form_factor"];
+                    eventData.WurflDeviceOS = device.Capabilities["device_os"] + " " + device.Capabilities["device_os_version"];
+
+                    Console.WriteLine(JsonConvert.SerializeObject(eventData));
+
+            } catch (WmException) {
+                errors.Add("Cannot get device information for user-agent: " + eventData.UserAgent);
+                Thread.Sleep(10000);
+            } catch (Exception e) {
+                errors.Add("Error: " + e.Message);
+                Thread.Sleep(10000);
+                }
+            if (errors.Count > 0) {
+                CreateErrorReport(errors);
+            }
+        }
+        reader.Close();
+    }
+
+        private static HttpRequest CreateRequest(EnrichedEventData eventData)
+        {
+            var request = new HttpRequestMock();
+            request.Headers.Add("Accept", eventData.Accept);
+            request.Headers.Add("User-Agent", eventData.UserAgent);
+            request.Headers.Add("X-Ucbrowser-Device-Ua", eventData.UcBrowserDeviceUa);
+            request.Headers.Add("X-Ucbrowser-Ua", eventData.UcBrowserUa);
+            return request;
+        }
+
+        private static void CreateErrorReport(List<String> errors)
+        {
+            using (StreamWriter report = new StreamWriter(@"error_report.txt"))
+            {
+                foreach (string err in errors)
+                {
+                    report.WriteLine(err);
+                }
+            }
+        }
+    }
+}
+

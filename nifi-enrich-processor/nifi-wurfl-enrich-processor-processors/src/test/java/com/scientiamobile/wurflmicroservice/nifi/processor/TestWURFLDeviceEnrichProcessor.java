d80abdf7cf43c0228bec4289ba41842d3a19a797
andrea <castello.a@gmail.com>
2021-01-18 16:22:28 +0100

    WPC-230: added unit tests

--- nifi-enrich-processor/nifi-wurfl-enrich-processor-processors/src/test/java/com/scientiamobile/wurflmicroservice/nifi/processor/TestWURFLDeviceEnrichProcessor.java
+++ nifi-enrich-processor/nifi-wurfl-enrich-processor-processors/src/test/java/com/scientiamobile/wurflmicroservice/nifi/processor/TestWURFLDeviceEnrichProcessor.java
@@ -1,5 +1,6 @@
 package com.scientiamobile.wurflmicroservice.nifi.processor;
 
+import com.scientiamobile.wurfl.wmclient.Model;
 import com.scientiamobile.wurfl.wmclient.WmClient;
 import com.scientiamobile.wurfl.wmclient.WmException;
 import org.apache.nifi.annotation.lifecycle.OnScheduled;
@@ -13,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -66,9 +68,95 @@ public class TestWURFLDeviceEnrichProcessor {
         assertEquals(failAttr, "Detection failed");
         List<MockFlowFile> success = testRunner.getFlowFilesForRelationship(WURFLDeviceEnrichProcessor.SUCCESS);
         assertEquals(0, success.size());
+    }
 
+    @Test
+    public void detectionUsingAttributeNameSuccess() throws WmException {
 
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.WM_SCHEME, "http");
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.WM_HOST, "localhost");
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.WM_PORT, "9080");
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.INPUT_ATTR_TYPE, "attribute name");
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.INPUT_ATTR_NAME, "User-Agent");
 
+        String ua = "Mozilla/5.0 (Linux; Android 10; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.62 Mobile Safari/537.36";
+        Map<String,String> headers = new HashMap<>();
+        headers.put("User-Agent", ua);
+        Model.JSONDeviceData d = createMockDevice();
+        when(wmClient.lookupHeaders(headers)).thenReturn(d);
+        final Map<String, String> attributes = new HashMap<>();
+
+        attributes.put("User-Agent", ua);
+
+        testRunner.enqueue(new byte[0], attributes);
+        testRunner.run();
+
+        List<MockFlowFile> failure = testRunner.getFlowFilesForRelationship(WURFLDeviceEnrichProcessor.FAILURE);
+        assertEquals(0, failure.size());
+        List<MockFlowFile> success = testRunner.getFlowFilesForRelationship(WURFLDeviceEnrichProcessor.SUCCESS);
+        assertNotNull(success);
+        assertEquals(1, success.size());
+        assertEquals("Google", success.get(0).getAttribute("wurfl.brand_name"));
+        assertEquals("Google Pixel 4 XL", success.get(0).getAttribute("wurfl.complete_device_name"));
+        assertEquals("google_pixel_4_xl_ver1", success.get(0).getAttribute("wurfl.wurfl_id"));
+        assertEquals("true", success.get(0).getAttribute("wurfl.is_smartphone"));
+        assertEquals("false", success.get(0).getAttribute("wurfl.is_robot"));
+    }
+
+    @Test
+    public void detectionUsingAttributeNamePrefixSuccess() throws WmException {
+
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.WM_SCHEME, "http");
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.WM_HOST, "localhost");
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.WM_PORT, "9080");
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.INPUT_ATTR_TYPE, "attribute name prefix");
+        testRunner.setProperty(WURFLDeviceEnrichProcessor.INPUT_ATTR_NAME, "http.headers.");
+
+        String ua = "Mozilla/5.0 (Linux; Android 10; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.62 Mobile Safari/537.36";
+        Map<String,String> headers = new HashMap<>();
+        headers.put("User-Agent", ua);
+        Model.JSONDeviceData d = createMockDevice();
+        when(wmClient.getImportantHeaders()).thenReturn(mockImportantHeaders());
+        when(wmClient.lookupHeaders(headers)).thenReturn(d);
+        final Map<String, String> attributes = new HashMap<>();
+
+        attributes.put("http.headers.User-Agent", ua);
+        attributes.put("http.headers.Connection", "keep-alive");
+        attributes.put("http.headers.Accept-Encoding", "gzip");
+
+        testRunner.enqueue(new byte[0], attributes);
+        testRunner.run();
+
+        List<MockFlowFile> failure = testRunner.getFlowFilesForRelationship(WURFLDeviceEnrichProcessor.FAILURE);
+        assertEquals(0, failure.size());
+        List<MockFlowFile> success = testRunner.getFlowFilesForRelationship(WURFLDeviceEnrichProcessor.SUCCESS);
+        assertNotNull(success);
+        assertEquals(1, success.size());
+        assertEquals("Google", success.get(0).getAttribute("wurfl.brand_name"));
+        assertEquals("Google Pixel 4 XL", success.get(0).getAttribute("wurfl.complete_device_name"));
+        assertEquals("google_pixel_4_xl_ver1", success.get(0).getAttribute("wurfl.wurfl_id"));
+        assertEquals("true", success.get(0).getAttribute("wurfl.is_smartphone"));
+        assertEquals("false", success.get(0).getAttribute("wurfl.is_robot"));
+    }
+
+    private String[] mockImportantHeaders() {
+        String[] importantHeaders = new String[4];
+        importantHeaders[0]  = "User-Agent";
+        importantHeaders[1] = "Device-Stock-UA";
+        importantHeaders[2] = "X-OperaMini-Phone-UA";
+        importantHeaders[3] = "X-UCBrowser-Device-UA";
+        return importantHeaders;
+    }
+
+    private Model.JSONDeviceData createMockDevice() {
+        Map<String, String> capabilities = new HashMap<>();
+        capabilities.put("brand_name", "Google");
+        capabilities.put("complete_device_name", "Google Pixel 4 XL");
+        capabilities.put("wurfl_id", "google_pixel_4_xl_ver1");
+        capabilities.put("is_smartphone", "true");
+        capabilities.put("is_robot", "false");
+        Model.JSONDeviceData d = new Model().new JSONDeviceData(capabilities, "", 0);
+        return d;
     }
 
     class TestableWURFLDeviceEnrich extends WURFLDeviceEnrichProcessor {
